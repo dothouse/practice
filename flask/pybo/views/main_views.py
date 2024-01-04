097f70fe7f0abc6710c3dc84e8061da506174d58
dothouse <jental222@gmail.com>
2024-01-04 18:37:09 +0900

    project start

--- flask/pybo/views/main_views.py
+++ flask/pybo/views/main_views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
+from pybo.models import Question
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -8,8 +10,19 @@ def hello_pybo():
 
 @bp.route('/')
 def index():
-    return '메인페이지 - 인덱스'
+    question_list = Question.query.order_by(Question.create_date.desc())
+    return render_template('question/question_list.html',
+                           question_list = question_list)
 
 @bp.route('/test')
 def testpage():
     return '연습용 페이지'
+
+@bp.route('/detail/<int:question_id>/')
+def detail(question_id):
+    question = Question.query.get_or_404(question_id)
+    return render_template('question/question_detail.html',
+                           question = question)
+
+
+### 2-5 질문 목록, 질문 상세 기능 분리하기
