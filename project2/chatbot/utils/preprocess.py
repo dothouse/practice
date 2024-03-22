b23815cc5f2fee7ca397208e5c62091db6d582dc
dothouse <jental222@gmail.com>
2024-03-22 16:35:05 +0900

    20240322

--- project2/chatbot/utils/preprocess.py
+++ project2/chatbot/utils/preprocess.py
@@ -2,13 +2,28 @@ from konlpy.tag import Komoran
 import pickle
 
 class Preprocess:
-    def __init__(self, userdic=None): # userdic 인자에는 사용자 정의 사전 파일 경로 입력가능
-        
+
+    # 클래스가 시작될때 __init__을 불러오는
+    # 그래서 워드 인덱스랑 유저사전을 불러와서 
+    def __init__(self, word2index_dic='D:/song/practice/project2/chatbot/train_tools/dict/chatbot_dict.bin' ,userdic='user_dic.tsv'): # userdic 인자에는 사용자 정의 사전 파일 경로 입력가능
+        # 단어 인덱스 사전 불러오기
+        if(word2index_dic != ''):
+            f = open(word2index_dic, "rb")
+            self.word_index = pickle.load(f)
+            f.close()
+            print("success load word_dic")
+        else:
+            self.word_index = None
+            print("fail load word_dic")
+
         # 형태소 분석기 초기화
         self.komoran = Komoran(userdic=userdic)
 
         # 제외할 품사
         # 참조 : https://docs.komoran.kr/firststep/postypes.html
+        # 관계언 제거, 기호 제거
+        # 어미 제거
+        # 접미사 제거
         self.exclusion_tags = [
             'JKS', 'JKC', 'JKG', 'JKO', 'JKB', 'JKV', 'JKQ',
             # 주격조사, 보격조사, 관형격조사, 목적격조사, 부사격조사, 호격조사, 인용격조사
@@ -23,11 +38,11 @@ class Preprocess:
         ]
 
 
-    # Komoran POS tagging
+    # 형태소 분석기 POS 태거
     def pos(self, sentence):
         return self.komoran.pos(sentence)
 
-    # 불용어 제거
+    # 불용어 제거 후 필요한 품사 정보만 가져오기
     def get_keywords(self, pos, without_tag=False):
         f = lambda x: x in self.exclusion_tags
         word_list = []
@@ -35,3 +50,16 @@ class Preprocess:
             if f(p[1]) is False:
                 word_list.append(p if without_tag is False else p[0])
         return word_list
+
+    # 키워드를 단어 인덱스 시퀀스로 변환
+    def get_wordidx_sequence(self, keywords):
+        if self.word_index is None:
+            return []
+        w2i = []
+        for word in keywords:
+            try:
+                w2i.append(self.word_index[word])
+            except KeyError:
+                # 해당 단어가 사전에 없는 경우 OOV 처리
+                w2i.append(self.word_index['OOV'])
+        return w2i
